19ef5e38fa2e165e412a83647230b1f09e1b5190
Peter Hutterer <peter.hutterer@who-t.net>
2019-07-05 10:21:40 +1000

    Change the server script to be more generic
    We don't want to rebuild every time we start the debug server

---
+++ serverctl.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+srvroot=server/
+srvpath=$srvroot/build/
+
+export PYTHONPATH=$PWD/$srvroot:$PYTHONPATH
+
+rebuild() {
+    # Use a podman container to set up a python flask server based on our API
+    # description.
+
+    # --rm ... remove container when finished
+    # --volume path/to/a/:/path/to/b ... map a to b in the container
+    # openapitools/ ...  the container to run
+    # generate [...] ...  the command to invoke in the container
+    podman run --rm \
+        --volume ${PWD}/server/:/server:Z \
+        openapitools/openapi-generator-cli \
+        generate -i /server/tuhiwui.yaml \
+        -g python-flask -o /$srvpath
+    if [[ $? -ne 0 ]]; then
+         exit $?
+    fi
+}
+
+run() {
+        pushd $srvpath > /dev/null
+        python3 -m openapi_server
+        popd > /dev/null
+}
+
+rebuild=0
+run=0
+
+while ! [[ -z "$1" ]]; do
+    case $1 in
+        --rebuild)
+            rebuild=1
+            shift
+            ;;
+        --run)
+            run=1
+            shift
+            ;;
+        **)
+            echo "Invalid argument: $1"
+            exit 1
+            ;;
+    esac
+done
+
+[[ $rebuild == 1 ]] && rebuild
+[[ $run == 1 ]] && run
